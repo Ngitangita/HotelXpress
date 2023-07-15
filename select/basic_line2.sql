8770609470acad4a965672898cacc45108267d14
Hevitriniavo <hei.tantely@gmail.com>
2023-07-15 15:22:27 +0300

    Fix: I modify a line 2 query

--- select/basic_line2.sql
+++ select/basic_line2.sql
@@ -1,6 +1,22 @@
-SELECT reservation.* FROM reservation 
-INNER JOIN reservation_contain ON reservation.id = reservation_contain.reservation_id
-INNER JOIN room ON reservation_contain.room_id = room.id 
-INNER JOIN hotel ON hotel.id = room.hotel_id
-WHERE hotel.id = 1
-ORDER BY reservation.date_reservation DESC;
+SELECT
+    r.*
+FROM
+    reservation AS r
+INNER JOIN
+    reservation_contain AS rc
+ON
+    r.id = rc.reservation_id
+INNER JOIN
+    room
+ON
+    rc.room_id = room.id
+INNER JOIN
+    hotel
+ON
+    hotel.id = room.hotel_id
+WHERE
+    room.hotel_id = 1
+ORDER BY
+    r.date_reservation
+DESC;
+--- NB: x = un hotel donné
