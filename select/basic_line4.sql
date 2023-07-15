c5d1663ee883a184739be9986bef723244ef6f1b
Hevitriniavo <hei.tantely@gmail.com>
2023-07-15 15:29:25 +0300

    feat: I modify a line 4 query

--- select/basic_line4.sql
+++ select/basic_line4.sql
@@ -1,5 +1,18 @@
-SELECT * FROM promotion INNER JOIN have_promotion 
-ON promotion.id = have_promotion.promotion_id
-INNER JOIN hotel 
-ON have_promotion.hotel_id = hotel.id
-WHERE  promotion.start_date >=  date1  AND promotion.end_date  <= date2;
+SELECT
+    promotion.*
+FROM
+    promotion
+INNER JOIN
+    have_promotion
+ON
+    have_promotion.promotion_id = promotion.id
+INNER JOIN
+    hotel
+ ON
+    have_promotion.hotel_id = hotel.id
+WHERE
+    promotion.start_date >= date1
+AND
+    promotion.end_date <= date2:
+
+--- date1 et date2 interval nommé  periode
